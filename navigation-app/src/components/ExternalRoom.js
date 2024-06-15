0ec0a4404291617ed4cf24dcb4b5d51e81f6e4d8
judyfromearth <166911645+judyfromearth@users.noreply.github.com>
2024-06-15 15:40:38 +0900

    Update ExternalRoom.js

--- navigation-app/src/components/ExternalRoom.js
+++ navigation-app/src/components/ExternalRoom.js
@@ -22,7 +22,7 @@ const ExternalRoom = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === '42.195') {
+    if (password === '42195') {
       localStorage.setItem('externalRoomUnlocked', 'true');
       setIsUnlocked(true);
       navigate('/room/외부인의 방/next');
